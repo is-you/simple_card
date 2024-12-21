aa464dfba1c482a75e343d6d29da937a1eaad22a
is-you <amonisyou@ya.ru>
2024-12-22 01:24:57 +0300

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,16 @@
+function createSlider() {
+    var swiper = new Swiper(".swiper", {
+        slidesPerView: "auto",
+        spaceBetween: 20,
+        centeredSlides: true,
+        pagination: {
+            clickable: true,
+        },
+    });
+}
+
+createSlider();
+
+function checkMobile(){
+
+}
